6bcea28270287317eb767d4a820472a227c82025
bahodur <nazarovbahodur1999@gmail.com>
2023-09-07 11:11:34 +0500

    created two new struct UpdateItem and NewItem

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -1,1 +1,13 @@
 package model
+
+type NewItem struct {
+	Title  string `json:"title"`
+	Owner  string `json:"owner"`
+	Rating int    `json:"rating"`
+}
+
+type UpdateItem struct {
+	Title  *string `json:"title"`
+	Owner  *string `json:"owner"`
+	Rating *int    `json:"rating"`
+}
