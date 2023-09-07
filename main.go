156c55da28482386e26a7ce48de1376764bcc14a
bahodur <nazarovbahodur1999@gmail.com>
2023-09-07 10:57:51 +0500

    created item struct

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
-package GraphQl_CRUD
+package main
+
+import "os"
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	initDatabase()
+	createTable()
+
+	defer databasa.DBConn.Close()
+
+	srv :=
+}
